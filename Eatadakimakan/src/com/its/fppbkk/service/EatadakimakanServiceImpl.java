4dac3106fd3cfff796bba9c74101b1528961943d
Rimba Azhara <rimbaazhara55@gmail.com>
2019-05-19 11:42:33 +0700

    servis

--- Eatadakimakan/src/com/its/fppbkk/service/EatadakimakanServiceImpl.java
+++ Eatadakimakan/src/com/its/fppbkk/service/EatadakimakanServiceImpl.java
@@ -2,31 +2,38 @@ package com.its.fppbkk.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.its.fppbkk.dao.RestoranDAO;
 import com.its.fppbkk.entity.Restoran;
 
 public class EatadakimakanServiceImpl implements EatadakimakanService {
 
+	@Autowired
+	private RestoranDAO restoranDAO;
+	
 	@Override
 	public List<Restoran> getRestoran() {
 		// TODO Auto-generated method stub
-		return null;
+		return restoranDAO.getRestoran();
 	}
 
 	@Override
 	public void saveRestoran(Restoran resto) {
 		// TODO Auto-generated method stub
-
+		restoranDAO.saveRestoran(resto);
 	}
 
 	@Override
 	public Restoran getRestoranByID(int restoID) {
 		// TODO Auto-generated method stub
-		return null;
+		return restoranDAO.getRestoranByID(restoID);
 	}
 
 	@Override
 	public void deleteRestoran(int restoID) {
 		// TODO Auto-generated method stub
+		restoranDAO.deleteRestoran(restoID);
 
 	}
 
